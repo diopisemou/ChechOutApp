58afa0d257d9ac86e9812ec2383cd5425981a04f
Mouhamadou Tidiane El Bachir Diop <diopisemou@gmail.com>
2018-03-06 17:54:04 +0100

    Footer Implemented

---
+++ ChechOutApp/ChechOutApp/Views/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChechOutApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private void TapGestureRecognizer_OnTappedHome(object sender, EventArgs e)
+	    {
+	        DisplayAlert("Home", "Home Button Tapped", "Ok");
+	    }
+
+	    private void TapGestureRecognizer_OnTappedSearch(object sender, EventArgs e)
+	    {
+	        DisplayAlert("Search", "Search Button Tapped", "Ok");
+	    }
+
+	    private void TapGestureRecognizer_OnTappedMenu(object sender, EventArgs e)
+	    {
+	        DisplayAlert("Menu", "Menu Button Tapped", "Ok");
+	    }
+
+	    private void TapGestureRecognizer_OnTappedCart(object sender, EventArgs e)
+	    {
+	        DisplayAlert("Cart", "Cart Button Tapped", "Ok");
+	    }
+
+	    private void TapGestureRecognizer_OnTappedAccount(object sender, EventArgs e)
+	    {
+	        DisplayAlert("Account", "Account Button Tapped", "Ok");
+	    }
+
+    }
+}
